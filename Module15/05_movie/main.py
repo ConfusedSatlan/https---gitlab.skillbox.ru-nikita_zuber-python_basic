af10e5c1f8059becce7e8a8ddbb53296717f669c
Alexa <aguseva@mail.com>
2021-03-31 21:09:36 +0200

    решения нужно доработать

--- Module15/05_movie/main.py
+++ Module15/05_movie/main.py
@@ -6,12 +6,12 @@ text = input('Введите название фильма: ')
 flag = False
 
 while text != 'end':
-    for i in range(0, len(films), 1):
+    for i in range(0, len(films), 1):  # TODO этот интервал (range) нужно записать короче (три аргумета здесь не нужны)
         if films[i] == text and text not in new_film_list:
             new_film_list.append(films[i])
-            flag = True
+            flag = True  # TODO если пользователь ввёл дубликат - сообщение должно быть не 'Ошибка! Такого фильма нету!'
     
-    if flag != True:  
+    if flag is not True:
         print('Ошибка! Такого фильма нету!')
 
     flag = False
